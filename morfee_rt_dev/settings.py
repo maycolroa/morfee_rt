8f42e93e87b46bf7c86e1bf173ae2c2a87e421c0
araxomed-morfee <daniel.franco@innovacionanalitica.co>
2023-08-30 15:43:10 -0500

    Storages.

--- morfee_rt_dev/settings.py
+++ morfee_rt_dev/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'morfee_rt_dev',
     'consulta',
     'autorizaciones',
